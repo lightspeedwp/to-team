fb16e9974a9eabba2b6088d775b927c2513d7214
Fernando Tessmann <nandotess85@gmail.com>
2016-12-09 10:05:35 -0200

    reduced the access to server (check API key status) using transients + made the API URLs dev/live dynamic using a prefix “dev-“ in the API KEY

--- to-team.php
+++ to-team.php
@@ -4,7 +4,7 @@
  * Plugin URI:	https://www.lsdev.biz/product/tour-operator-team/
  * Description:	Real peoples' faces go a long way to building trust with your clients. The Team Extension allows your business's staff to be added as Team Members with their own profile which can be associated with specific destinations and tours.
  * Author:		LightSpeed
- * Version: 	1.0.0
+ * Version: 	1.0.1
  * Author URI: 	https://www.lsdev.biz/
  * License: 	GPL3+
  * Text Domain: to-team
@@ -19,7 +19,7 @@ if ( ! defined( 'WPINC' ) ) {
 define('TO_TEAM_PATH',  plugin_dir_path( __FILE__ ) );
 define('TO_TEAM_CORE',  __FILE__ );
 define('TO_TEAM_URL',  plugin_dir_url( __FILE__ ) );
-define('TO_TEAM_VER',  '1.0.0' );
+define('TO_TEAM_VER',  '1.0.1' );
 
 if(!defined('TEAM_ARCHIVE_URL')){
 	define('TEAM_ARCHIVE_URL',  'team-members' );
@@ -68,7 +68,7 @@ function to_team_api_admin_init(){
 	}
 	$api_array = array(
 		'product_id'	=>		'TO Team',
-		'version'		=>		'1.0.0',
+		'version'		=>		'1.0.1',
 		'instance'		=>		$instance,
 		'email'			=>		$data['email'],
 		'api_key'		=>		$data['api_key'],
