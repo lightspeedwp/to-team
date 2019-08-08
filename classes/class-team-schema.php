dec633a459274b61e746d0da5599e45990fdda55
JosephHart <joe@lsdev.biz>
2019-08-08 11:21:32 +0200

    Travis fixes

--- classes/class-team-schema.php
+++ classes/class-team-schema.php
@@ -34,37 +34,37 @@ class LSX_TO_Team_Schema extends LSX_TO_Team {
 	 */
 	public function team_single_schema() {
 		if ( is_singular( 'team' ) ) {
-			$pinterest = get_post_meta( get_the_ID(), 'pinterest', true );
+			$pinterest  = get_post_meta( get_the_ID(), 'pinterest', true );
 			$googleplus = get_post_meta( get_the_ID(), 'googleplus', true );
-			$twitter = get_post_meta( get_the_ID(), 'twitter', true );
-			$linkedin = get_post_meta( get_the_ID(), 'linkedin', true );
-			$facebook = get_post_meta( get_the_ID(), 'facebook', true );
-			$phone_number_team = get_post_meta( get_the_ID(), 'contact_number', true );
-			$job_role_team = get_post_meta( get_the_ID(), 'role', true );
+			$twitter    = get_post_meta( get_the_ID(), 'twitter', true );
+			$linkedin   = get_post_meta( get_the_ID(), 'linkedin', true );
+			$facebook   = get_post_meta( get_the_ID(), 'facebook', true );
+			$phone_team = get_post_meta( get_the_ID(), 'contact_number', true );
+			$job_role   = get_post_meta( get_the_ID(), 'role', true );
 			$email_team = get_post_meta( get_the_ID(), 'contact_email', true );
-			$name_team = get_the_title();
-			$team_description = wp_strip_all_tags( get_the_content() );
+			$name_team  = get_the_title();
+			$team_descp = wp_strip_all_tags( get_the_content() );
 
-		if (! empty($googleplus) || ! empty($pinterest) || ! empty($twitter) || ! empty($linkedin) ||  ! empty($facebook) ) {
-			$social_array = array( $googleplus, $pinterest, $twitter, $linkedin, $facebook );
+			if ( ! empty( $googleplus ) || ! empty( $pinterest ) || ! empty( $twitter ) || ! empty( $linkedin ) || ! empty( $facebook ) ) {
+				$social_array = array( $googleplus, $pinterest, $twitter, $linkedin, $facebook );
 		}
 
-		$meta = array(
-		"@context" => "http://schema.org/",
-		"@type" => "Person",
-		"email" => $email_team,
-		"jobTitle" => $job_role_team,
-		"telephone" => $phone_number_team,
-		"description" => $team_description,
-		"name" => $name_team,
-		"sameAs" => $social_array,
-		);
-		$output = wp_json_encode( $meta, JSON_UNESCAPED_SLASHES  );
-		?>
+			$meta   = array(
+				'@context'    => 'http://schema.org/',
+				'@type'       => 'Person',
+				'email'       => $email_team,
+				'jobTitle'    => $job_role,
+				'telephone'   => $phone_team,
+				'description' => $team_descp,
+				'name'        => $name_team,
+				'sameAs'      => $social_array,
+			);
+			$output = wp_json_encode( $meta, JSON_UNESCAPED_SLASHES );
+			?>
 		<script type="application/ld+json">
 			<?php echo wp_kses_post( $output ); ?>
 		</script>
-		<?php
+			<?php
 		}
 	}
 }
