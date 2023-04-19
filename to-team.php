efd6f75c9cc9c8e54c9591317c7e5391da3bfdd1
Warwick <krugazul@gmail.com>
2023-04-19 23:31:53 +0200

    Updating the version number and stable tag.

--- to-team.php
+++ to-team.php
@@ -3,7 +3,7 @@
  * Plugin Name:	LSX Tour Operator Team
  * Plugin URI:	https://www.lsdev.biz/product/tour-operator-team/
  * Description:	Real peoples' faces go a long way to building trust with your clients. The Team Extension allows your business's staff to be added as Team Members with their own profile which can be associated with specific destinations and tours.
- * Version:     1.2.5
+ * Version:     1.2.6
  * Author:      LightSpeed
  * Author URI: 	https://www.lsdev.biz/
  * License: 	GPL3+
@@ -20,7 +20,7 @@ if ( ! defined( 'WPINC' ) ) {
 define( 'LSX_TO_TEAM_PATH', plugin_dir_path( __FILE__ ) );
 define( 'LSX_TO_TEAM_CORE', __FILE__ );
 define( 'LSX_TO_TEAM_URL', plugin_dir_url( __FILE__ ) );
-define( 'LSX_TO_TEAM_VER', '1.2.5' );
+define( 'LSX_TO_TEAM_VER', '1.2.6' );
 
 if ( ! defined( 'TEAM_ARCHIVE_URL' ) ) {
 	define( 'TEAM_ARCHIVE_URL', 'team-members' );
