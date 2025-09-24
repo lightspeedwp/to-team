5491b7ee1ff271e9bd0264a0afe77afc9bd22b0b
Ashley Shaw <ashley@lightspeedwp.agency>
2025-09-25 00:29:15 +0700

    Refactor code structure for improved readability and maintainability

---
+++ assets/js/to-team-admin.js
@@ -0,0 +1,12 @@
+/**
+ * TO Team Admin JavaScript
+ */
+
+( function( $ ) {
+	'use strict';
+
+	$( document ).ready( function() {
+		// Initialize TO Team admin functionality
+	} );
+
+} )( jQuery );
---
+++ assets/js/to-team.js
@@ -0,0 +1,12 @@
+/**
+ * TO Team Frontend JavaScript
+ */
+
+( function( $ ) {
+	'use strict';
+
+	$( document ).ready( function() {
+		// Initialize TO Team functionality
+	} );
+
+} )( jQuery );
